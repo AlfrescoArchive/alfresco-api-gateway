f15841528f4d6ea6b01743901077ae9412c5465b
Ryan Dawson <ryan.dawson@alfresco.com>
2017-09-07 10:33:35 +0100

    different approach to cors filter

--- src/main/java/org/activiti/cloud/gateway/GatewayApplication.java
+++ src/main/java/org/activiti/cloud/gateway/GatewayApplication.java
@@ -28,18 +28,5 @@ public class GatewayApplication {
         return new KeycloakFilterRoute();
     }
 
-    @Bean
-    public FilterRegistrationBean corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-        bean.setOrder(0);
-        return bean;
-    }
 
 }

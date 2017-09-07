c97bc9b4e1dedefc320dfaf2dd2d3170aee92b3a
Ryan Dawson <ryan.dawson@alfresco.com>
2017-09-07 09:29:49 +0100

    add cors settings for kub setup

--- src/main/java/org/activiti/cloud/gateway/GatewayApplication.java
+++ src/main/java/org/activiti/cloud/gateway/GatewayApplication.java
@@ -3,9 +3,13 @@ package org.activiti.cloud.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -22,6 +26,20 @@ public class GatewayApplication {
     @Bean
     public KeycloakFilterRoute keycloakFilterRoute() {
         return new KeycloakFilterRoute();
+    }
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
     }
 
 }

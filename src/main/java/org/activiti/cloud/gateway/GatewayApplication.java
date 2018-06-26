91b19ab25656a2dc9798eaeff974e7ad28baf7fa
Ryan Dawson <ryan.dawson@alfresco.com>
2018-06-26 09:30:33 +0100

    udpate with cors configuration

--- src/main/java/org/activiti/cloud/gateway/GatewayApplication.java
+++ src/main/java/org/activiti/cloud/gateway/GatewayApplication.java
@@ -7,8 +7,14 @@ import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.handler.RoutePredicateHandlerMapping;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Arrays;
+import java.util.HashMap;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -27,5 +33,28 @@ public class GatewayApplication {
                                                                              DiscoveryLocatorProperties properties) {
         return new DiscoveryClientRouteDefinitionLocator(discoveryClient,
                 properties);
+    }
+
+    // see https://github.com/spring-cloud/spring-cloud-gateway/issues/229
+    @Bean
+    public CorsConfiguration corsConfiguration(RoutePredicateHandlerMapping routePredicateHandlerMapping) {
+        CorsConfiguration corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
+        Arrays.asList(HttpMethod.OPTIONS, HttpMethod.PUT, HttpMethod.GET, HttpMethod.DELETE, HttpMethod.POST, HttpMethod.PATCH).
+                forEach(m -> corsConfiguration.addAllowedMethod(m));
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("Authorization");
+        corsConfiguration.addAllowedHeader("authorization");
+        corsConfiguration.addAllowedHeader("Content-Type");
+        corsConfiguration.addAllowedHeader("Cache-Control");
+        corsConfiguration.addAllowedHeader("X-Requested-With");
+        corsConfiguration.addAllowedHeader("acccept");
+        corsConfiguration.addAllowedHeader("Origin");
+        corsConfiguration.addAllowedHeader("Access-Control-Request-Method");
+        corsConfiguration.addAllowedHeader("Access-Control-Request-Headers");
+        corsConfiguration.addAllowedHeader("X-CSRF-Token");
+        corsConfiguration.addExposedHeader("Access-Control-Allow-Origin");
+        corsConfiguration.addExposedHeader("Access-Control-Allow-Credentials");
+        routePredicateHandlerMapping.setCorsConfigurations(new HashMap<String, CorsConfiguration>() {{ put("/**", corsConfiguration); }});
+        return corsConfiguration;
     }
 }
